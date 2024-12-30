d40f8726b842b64ffee881701bf5d09c6b240d56
nyquil1x <gamboa.family.8@gmail.com>
2024-12-29 21:03:23 -0800

    Modularized the repetitive stuff

--- src/main.cpp
+++ src/main.cpp
@@ -8,56 +8,66 @@
 
 using namespace boost::filesystem;
 
+void copyTemplateFiles(const path &templatePath, const path &destinationPath) {
+    std::ifstream templateFile(templatePath.string());
+    if (!templateFile) {
+        std::cerr << "Error: Unable to open the template file: " << templatePath
+                  << std::endl;
+        return;
+    }
+
+    std::ofstream destinationFile(destinationPath.string());
+    if (!destinationFile) {
+        std::cerr << "Error: Unable to create the destination file: "
+                  << destinationPath << std::endl;
+        return;
+    }
+    std::string line;
+
+    while (std::getline(templateFile, line)) {
+        destinationFile << line << std::endl;
+    }
+
+    std::cout << "Destination file created: " << destinationPath << std::endl;
+}
+
+void initializeProjectFiles(const path &projectPath,
+                            const std::vector<std::string> &directories,
+                            const std::vector<std::string> &files) {
+    for (size_t i = 0; i < directories.size(); ++i) {
+        path dirPath = projectPath / directories[i];
+        create_directories(dirPath);
+
+        path templatePath = "./templates/" + files[i];
+        path filePath = dirPath / files[i];
+        copyTemplateFiles(templatePath, filePath);
+    }
+
+    std::vector<std::string> globalFiles = {"cppcrate.toml", "Makefile"};
+    for (auto const &globalFile : globalFiles) {
+        path templatePath = "./templates/" + globalFile;
+        path filePath = projectPath / globalFile;
+        copyTemplateFiles(templatePath, filePath);
+    }
+}
+
 int main(int argc, char *argv[]) {
     std::vector<std::string> directories = {"/include", "/src", "/tests"};
-    std::vector<std::string> files = {"example.h", "main.cpp", "test_main.cpp",
-                                      "cppcrate.toml", "Makefile"};
+    std::vector<std::string> files = {"example.h", "main.cpp", "test_main.cpp"};
 
-    if (argc < 2) {
+    if (argc < 3) {
+        std::cerr << "Usage: cppcrate new <projectname>" << std::endl;
         return 1;
     }
-    if (strcmp(argv[1], "new") < 1) {
-        std::cout << "I like bear" << std::endl;
-        for (int i = 0; i < 3; ++i) {
-            path template_path = "./templates/" + files[i];
-            path p(argv[2]);
-            path newPath = p / directories[i];
-            create_directories(newPath);
-            path filePath = newPath / files[i];
-            std::ofstream os(filePath.string());
-            path toml_path = p / files[3];
-            path toml_template_path = "./templates/" + files[3];
-            std::ofstream crate(toml_path.string());
-            std::cout << toml_path.string() << std::endl;
-            path makefile_path = p / files[4];
-            path makefile_template_path = "./templates/" + files[4];
-            std::ofstream make(makefile_path.string());
-            std::ifstream template_file(template_path.string());
-            if (!template_file) {
-                std::cerr << "Error: Unable to open the file: "
-                          << template_path.string() << std::endl;
-                return 1;
-            }
-            std::ofstream new_file(filePath.string());
-            std::cout << filePath.string() << std::endl;
-            std::string line;
-            while (std::getline(template_file, line)) {
-                std::cout << "Writing: " << line << std::endl;
-                new_file << line << std::endl;
-            }
-            std::ifstream toml_template(toml_template_path.string());
-            std::ofstream new_toml(toml_path.string());
-            while (std::getline(toml_template, line)) {
-                new_toml << line << std::endl;
-            }
-            std::ifstream makefile_template(makefile_template_path.string());
-            std::ofstream new_makefile(makefile_path.string());
-            while (std::getline(makefile_template, line)) {
-                new_makefile << line << std::endl;
-            }
-        }
 
+    if (std::string(argv[1]) == "new") {
+        path projectPath(argv[2]);
+        std::cout << "Initializing new project: " << projectPath << std::endl;
+        initializeProjectFiles(projectPath, directories, files);
     } else {
-        std::cout << "Not a valid argument" << std::endl;
+        std::cerr << "Error: Invalid Command. Use 'cppcrate new <projectname>'."
+                  << std::endl;
+        return 1;
     }
+    return 0;
 }
